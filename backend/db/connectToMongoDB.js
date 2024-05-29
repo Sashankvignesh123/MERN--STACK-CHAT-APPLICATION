30c3df85a6ed964bc00c89be8d6769cb5b7470c2
Sashankvignesh123 <133019596+Sashankvignesh123@users.noreply.github.com>
2024-05-29 22:09:10 +0530

    Deployment

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
-const url = process.env.MONGO_DB_URI || "0.0.0.0:27017";
+
 const connectToMongoDB = async () => {
-    try{
-        await mongoose.connect(`mongodb://${url}/chat-app`);
-        
-
-        console.log("connected to MongoDB");
-    }catch(error){
-        console.log("Error connecting to MongoDB",error.message);
-
-    }
-
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
 };
-export default connectToMongoDB; 
+
+export default connectToMongoDB;
